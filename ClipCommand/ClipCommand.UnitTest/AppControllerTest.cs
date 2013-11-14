1a9481f7d04ee5a23ddb17aa0b945f43e1fd4551
Alex Novak <alexwnovak@gmail.com>
2013-11-14 12:14:38 -0500

    First test for happy path in AppController.

---
+++ ClipCommand/ClipCommand.UnitTest/AppControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClipCommand.UnitTest
+{
+   [TestClass]
+   public class AppControllerTest
+   {
+      [TestMethod]
+      public void Run_NoArguments_ReturnsExitCodeZero()
+      {
+         var appController = new AppController();
+
+         int exitCode = appController.Run( null );
+
+         Assert.AreEqual( 0, exitCode );
+      }
+   }
+}
