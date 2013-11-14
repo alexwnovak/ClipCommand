75dc5dcc743877324eca46b01c87ec5afd8d5693
Alex Novak <alexwnovak@gmail.com>
2013-11-14 14:42:42 -0500

    +Winforms reference. Initial data send to real clipboard.

--- ClipCommand/ClipCommand/ClipboardAdapter.cs
+++ ClipCommand/ClipCommand/ClipboardAdapter.cs
@@ -1,10 +1,13 @@
-﻿namespace ClipCommand
+﻿using System.Text;
+using System.Windows.Forms;
+
+namespace ClipCommand
 {
    public class ClipboardAdapter : IClipboard
    {
       public void Write( byte[] data )
       {
-         throw new System.NotImplementedException();
+         Clipboard.SetText( Encoding.UTF8.GetString( data ) );
       }
    }
 }
